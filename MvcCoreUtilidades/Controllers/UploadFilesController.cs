9b730607d8e479af855b3b634627e8f43c6ca76d
marcospedroche05 <marcospedroche62@gmail.com>
2026-02-19 13:59:41 +0100

    STATIC FILES

---
+++ MvcCoreUtilidades/Controllers/UploadFilesController.cs
@@ -0,0 +1,33 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace MvcCoreUtilidades.Controllers
+{
+    public class UploadFilesController : Controller
+    {
+        public IActionResult SubirFile()
+        {
+            return View();
+        }
+        [HttpPost]
+        public async Task<IActionResult> SubirFile(IFormFile file)
+        {
+            //VAMOS A SUBIR A LOS ELEMENTOS TEMPORALES 
+            string tempFolder = Path.GetTempPath();
+            string fileName = file.FileName;
+
+            //Cuando pensamos en ficheros y en sus rutas 
+            //Pensamos en algo parecido a esto
+            //C:\Users\Usuario\Desktop\file.txt
+            //Net core no es windows y las de linux pueden ser distintas
+            //Debemos crear rutas siempre con herramientas de netcore para que sean compatibles con cualquier sistema operativo
+            string path = Path.Combine(tempFolder, fileName);
+            //PARA SUBIR FICHEROS SE UTILIZA STRING
+            using (Stream stream = new FileStream(path, FileMode.Create))
+            {
+                await file.CopyToAsync(stream);
+            }
+            ViewData["MENSAJE"] = "fichero subido a : " + path;
+            return View();
+        }
+    }
+}
