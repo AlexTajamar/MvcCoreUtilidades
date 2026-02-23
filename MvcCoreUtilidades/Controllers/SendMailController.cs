de6c522136648bb42f714a5edae201b5f223778f
marcospedroche05 <marcospedroche62@gmail.com>
2026-02-23 11:01:43 +0100

    encyptado

---
+++ MvcCoreUtilidades/Controllers/SendMailController.cs
@@ -0,0 +1,57 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using System.Net;
+using System.Net.Mail;
+
+namespace MvcCoreUtilidades.Controllers
+{
+   
+
+    public class SendMailController : Controller
+    {
+        private IConfiguration configuration;
+        public SendMailController(IConfiguration configuration)
+        {
+            this.configuration = configuration;
+        }
+        // GET: SendMailController
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public async Task<IActionResult> Index(string to,string subject,string body)
+        {
+            string user = this.configuration.GetValue<string>("MailSettings:Credentials:User");
+            MailMessage mail = new MailMessage();
+            mail.From = new MailAddress(user);
+            mail.To.Add(to);
+            mail.Subject = subject;
+            mail.Body = body;
+            mail.IsBodyHtml = true;
+            mail.Priority = MailPriority.Normal;
+
+            //RECUPERAMOS LOS DATOS
+            string password = this.configuration.GetValue<string>("MailSettings:Credentials:Password");
+            string host = this.configuration.GetValue<string>("MailSettings:Smtp:Host");
+            int port = this.configuration.GetValue<int>("MailSettings:Smtp:Port"); 
+            bool ssl = this.configuration.GetValue<bool>("MailSettings:Smtp:Ssl");
+            bool defaultCredentials = this.configuration.GetValue<bool>("MailSettings:Smtp:DefaultCredentials");
+            SmtpClient client = new SmtpClient();
+            client.Host = host;
+            client.Port = port;
+            client.EnableSsl = ssl;
+            client.UseDefaultCredentials = defaultCredentials;
+            NetworkCredential credentials = new NetworkCredential(user, password);
+            client.Credentials = credentials;
+            await client.SendMailAsync(mail);
+            ViewData["MENSAJE"] = "Cuerpo : " + body;
+
+            return View();
+        }
+
+        
+
+    }
+}
---
+++ MvcCoreUtilidades/Helpers/HelperCryptography.cs
@@ -0,0 +1,27 @@
+﻿using System.Security.Cryptography;
+using System.Text;
+
+namespace MvcCoreUtilidades.Helpers
+{
+    public class HelperCryptography
+    {
+        public static string Encrypt(string content)
+        {
+            //CIFRANDO EN UNA DIRECCION A NIVEL DE BYTES
+            byte[] input;
+            byte[] output;
+
+            //NECESITAMOS UNA CLASE PARA CONVERTIR DE BYTE A STRING Y DE STRING A BYTE
+            UnicodeEncoding encoding = new UnicodeEncoding();
+            //NECESITAMOS U OBJETO SHA 
+            SHA1 sha = SHA1.Create();
+           //CONVERTIMOS EL STRING A BYTE
+            input = encoding.GetBytes(content);
+            //CIFRAMOS EL CONTENIDO
+            output = sha.ComputeHash(input);
+            //CONVERTIMOS EL BYTE A STRING
+            return Convert.ToBase64String(output);
+
+        }
+    }
+}
